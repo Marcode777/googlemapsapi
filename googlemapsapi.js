8b4531f535b8c704cc6eb17d0acba7fcfaa0e34e
Marcode777 <marcosebello7@gmail.com>
2016-06-18 16:59:09 -0400

    now working

--- googlemapsapi.js
+++ googlemapsapi.js
@@ -1,16 +1,30 @@
-$(document).ready(function(){
+  // $(document).ready(function(){
    var map;
-
-  $ajax({
-    type:"GET",
-    url:"https://maps.googleapis.com/maps/api/js?key=AIzaSyDDXPKM3P2ZJPTh58O3KnXiG_QDYG0KuBg&callback=initMap"
-    success: function(data, x) {
-        x =  function initMap() {
+      function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
         });
       }
-      }
- })
-});
+    // });
+
+
+
+
+
+// $(document).ready(function(){
+//    var map;
+
+//   $ajax({
+//     type:"GET",
+//     url:"https://maps.googleapis.com/maps/api/js?key=AIzaSyDDXPKM3P2ZJPTh58O3KnXiG_QDYG0KuBg&callback=initMap"
+//     success: function(data, x) {
+//         x =  function initMap() {
+//         map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: -34.397, lng: 150.644},
+//           zoom: 8
+//         });
+//       }
+//       }
+//  })
+// });
